cb22213e854a45770304743d95cc2bf0bd69d981
Eibho <eibhyor@gmail.com>
2018-10-29 13:57:44 +0000

    Initial commit

---
+++ Assets/Scripts/Gun1.cs
@@ -0,0 +1,107 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gun1 : MonoBehaviour {
+    public float range = 1000f; //if player is whithin range it will hit
+    public GameObject bulletPoint; //where the raycast will shoot from
+    private bool m_fire;
+    private Character m_character;
+
+
+    private void Start()
+    {
+        m_character = GetComponent<Character>();
+    }
+
+    // Update is called once per frame
+    void Update () {
+
+        if (Input.GetButtonDown("1"))
+        {
+
+            m_character.Shoot(m_fire);
+            ShootRed();
+            
+        }
+
+        if (Input.GetButtonDown("2"))
+        {
+            m_character.Shoot(m_fire);
+            ShootBlue();
+
+        }
+
+        if (Input.GetButtonDown("3"))
+        {
+            m_character.Shoot(m_fire);
+            ShootGreen();
+            
+        }
+	}
+
+    void ShootRed()
+    {
+       
+
+        RaycastHit hit;
+        if (Physics.Raycast(bulletPoint.transform.position, bulletPoint.transform.forward, out hit, range)) //(position of bullet point,forward from bullet point position, shoot, within certain range)
+        {
+            Debug.Log(hit.transform.name);
+
+
+            ChangeMaterial changeMaterial = hit.transform.GetComponent<ChangeMaterial>();
+
+
+            if (changeMaterial != null)
+            {
+
+                changeMaterial.ColourRed();
+            }
+
+
+        }
+    }
+    void ShootBlue()
+    {
+        RaycastHit hit;
+        if (Physics.Raycast(bulletPoint.transform.position, bulletPoint.transform.forward, out hit, range)) //(position of bullet point,forward from bullet point position, shoot, within certain range)
+        {
+            Debug.Log(hit.transform.name);
+
+
+            ChangeMaterial changeMaterial = hit.transform.GetComponent<ChangeMaterial>();
+
+
+            if (changeMaterial != null)
+            {
+
+                changeMaterial.ColourBlue();
+            }
+
+
+        }
+    }
+
+    void ShootGreen()
+    {
+        RaycastHit hit;
+        if (Physics.Raycast(bulletPoint.transform.position, bulletPoint.transform.forward, out hit, range)) //(position of bullet point,forward from bullet point position, shoot, within certain range)
+        {
+            Debug.Log(hit.transform.name);
+
+
+            ChangeMaterial changeMaterial = hit.transform.GetComponent<ChangeMaterial>();
+            
+
+            if (changeMaterial != null)
+            {
+                
+                changeMaterial.ColourGreen();
+            }
+
+           
+        }
+    }
+
+}
---
+++ Assets/Scripts/Zombie/EnemyController.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI; 
+
+public class EnemyController : MonoBehaviour {
+
+    public float distanceFrom = 10f;
+
+    private Vector3 enemyTarget;
+    public float enemyRotation;
+
+    Transform player;
+    NavMeshAgent navAgent;
+
+	// Use this for initialization
+	void Start () {
+
+        player = GameObject.FindGameObjectWithTag("Player").transform;
+        
+        navAgent = GetComponent<NavMeshAgent>();
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        float distance = Vector3.Distance(player.position, transform.position);
+
+        if(distance <= distanceFrom)
+        {
+            navAgent.SetDestination(player.position);
+            FacePlayer();
+        }
+
+	}
+    
+    void FacePlayer()
+    {
+        enemyTarget = new Vector3(player.position.x + enemyRotation, transform.position.y, transform.position.z);
+
+
+        transform.LookAt(player);
+
+
+        transform.Rotate(new Vector3(0, 90f, 0));
+    }
+
+    
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.blue;
+        Gizmos.DrawWireSphere(transform.position, distanceFrom);
+    }
+}
