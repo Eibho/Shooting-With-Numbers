eda9b50b39fa185ab73470973cee473b73068662
Eibho <eibhyor@gmail.com>
2018-10-29 17:22:24 +0000

    Update Assets/Scripts/Zombie/ZombieAnimator.cs

---
+++ Assets/Scripts/Zombie/ZombieAnimator.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ZombieAnimator : MonoBehaviour {
+
+    private Animator m_animator;
+    public float m_forward;
+
+    
+    void Start () {
+        
+        m_animator = GetComponent<Animator>();
+    }
+
+    public void Move(float forward)
+    {
+        //walk animation
+        
+
+        m_animator.SetFloat("Forward", forward);
+    }
+}
